b65ba49343852916f9cbb2b89aba4f4568d1add9
hussamkhatib <hussamkhatib20@gmail.com>
2022-03-24 10:54:14 +0530

    refactor: convert Header component into tsx file and have a seperate file for its elemenst

---
+++ src/components/Header/elements.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  position: absolute;
+  color: #924d8b;
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './elements';
+
+function Header() {
+    return <Title>PokeWars</Title>;
+}
+
+export default Header;
