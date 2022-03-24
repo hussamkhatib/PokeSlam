daa6d902490b0ceb32c2daab39a1cad97ecc2f4a
hussamkhatib <hussamkhatib20@gmail.com>
2022-03-24 11:00:14 +0530

    refactor : add a seperate file for elements for Rule component

---
+++ src/components/Rule/elements.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+height: 100vh;
+display: grid;
+grid-template-rows: 4fr 1fr;
+`;
+
+export const ImageContainer = styled.div`
+background: #fff;
+`;
+
+export const Image = styled.img`
+height: 100%;
+max-width: 100%;
+display: block;
+margin: 0 auto;
+`;
+
+export const InputContainer = styled.div`
+margin: 0 auto;
+`;
+
+export const P = styled.p`
+margin: 0;
+padding: 0.6em 0;
+font-size: 1.2em;
+`;
+
+export const Button = styled.button`
+display: block;
+padding: 0.4em;
+`;
---
+++ src/components/Rule/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Hero from '../images/hero.png';
+import { Form, Image, ImageContainer, InputContainer, P, Button } from './elements';
+
+function Rule({ startGame, handleChange, totalCards }: any) {
+    console.log(totalCards);
+    return (
+        <Form onSubmit={startGame}>
+            <ImageContainer>
+                <Image height="400" width="600" src={Hero} alt="hero" />
+            </ImageContainer>
+            <InputContainer>
+                <P>Select no of cards</P>
+                <label>3</label>
+                <input
+                    type="radio"
+                    value="3"
+                    onChange={handleChange}
+                    name="totalCards"
+                    checked={totalCards === '3'}
+                />
+                <label>5</label>
+                <input
+                    type="radio"
+                    value="5"
+                    onChange={handleChange}
+                    name="totalCards"
+                    checked={totalCards === '5'}
+                />
+                <label>10</label>
+                <input
+                    type="radio"
+                    value="10"
+                    onChange={handleChange}
+                    name="totalCards"
+                    checked={totalCards === '10'}
+                />
+                <Button>Generate Cards</Button>
+            </InputContainer>
+        </Form>
+    );
+}
+
+export default Rule;
