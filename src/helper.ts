c0b274a3bba2964a71cfa8a24beec83befec3651
hussamkhatib <hussamkhatib20@gmail.com>
2022-04-04 22:52:16 +0530

    refactor: convert helper.js to helper.ts

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,6 @@
-export const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+export const random = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
 
-export function getPokemons(totalCards) {
+export function getPokemons(totalCards:number) {
     return Promise.all(
         Array.from({ length: totalCards }, () =>
             fetch(`https://pokeapi.co/api/v2/pokemon/${random(1, 800)}`).then((r) => r.json())
