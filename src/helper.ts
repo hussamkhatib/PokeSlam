7ddfcd3edcf403437fe622bde68b783bb77e2682
hussamkhatib <hussamkhatib20@gmail.com>
2022-05-06 21:08:59 +0530

    style: prettify

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,6 @@
 export const random = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
 
-export function getPokemons(totalCards:number) {
+export function getPokemons(totalCards: number) {
     return Promise.all(
         Array.from({ length: totalCards }, () =>
             fetch(`https://pokeapi.co/api/v2/pokemon/${random(1, 800)}`).then((r) => r.json())
